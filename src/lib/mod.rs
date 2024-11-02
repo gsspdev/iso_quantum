777abf1f9af1b400983988b4efec4ebd61470a35
gsspdev <dev@gssp.io>
2024-11-01 22:53:27 -0700

    experimenting with binary representations

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,13 +1,28 @@
 use deku::bitvec::BitVec;
+use bitvec::prelude::Msb0;
 
 type BitArray = [u8; 4];
 
 const ARR: BitArray = [0, 1, 2, 3];
-const BITS: BitVec<u8, Msb0> = BitVec::from_slice(&ARR);
 const DATA: u16 = 0x2021u16;
 
 pub fn test_print_data() {
+    let bits: BitVec<u8, Msb0> = BitVec::from_slice(&ARR).clone();
+
+    let slice0: BitVec<u8, Msb0> = BitVec::from_element(0);
+    let slice1: BitVec<u8, Msb0> = BitVec::from_element( 1);
+    let slice2: BitVec<u8, Msb0> = BitVec::from_element( 2);
+    let slice3: BitVec<u8, Msb0> = BitVec::from_element( 3);
+
     println!("ARR: {:?}", ARR);
-    println!("BITS: {:?}", BITS);
+
+    println!("slice: {:?}", slice0);
+    println!("slice: {:?}", slice1);
+    println!("slice: {:?}", slice2);
+    println!("slice: {:?}", slice3);
+
+    println!("{}", ("putting these together").to_string());
+
+    println!("bits: {:?}", bits);
     println!("DATA: {:#06x}", DATA);
 }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod lib;
+use crate::lib::test_print_data;
+
+fn main() {
+    test_print_data();
+}
